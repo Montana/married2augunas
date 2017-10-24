f0a40d2f54d7d3788636a563f9affabe09c855e2
Montana Mendy <montanamendy@gmail.com>
2017-10-24 12:49:52 -0700

    Create married2augunas.js

---
+++ married2augunas.js
@@ -0,0 +1,6 @@
+var person = prompt("Please enter your name", "Augunas");
+
+if (person != null) {
+    document.getElementById("married2augunas").innerHTML =
+    "Hello " + person + "! Hello Ms. Augunas?";
+}
